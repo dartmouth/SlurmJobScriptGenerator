8d690f1788963164a98af0fba46f06e12d7d2c7f
Bonnie J. Scott <bonnie.j.scott@dartmouth.edu>
2022-10-13 18:43:26 -0400

    node info output is now in JSON format; not yet de-duped

--- cloverview.py
+++ cloverview.py
@@ -1,34 +1,16 @@
 #!/usr/bin/python3.6
-import subprocess, array
-##sinfo -h --format="[%n--%c CPUs,%Y Cores|Memory=%m,State=%a,Load=%O,partition:%P\")" #human-readable
-#====================================================
-#sinfo_data is formatted as a python array: no easy way to use preformatted array, might as well just CSV it?
-# sinfo -h --format="[%n,%c,%Y,%m,%a,%O,%P]" | sort -r
-#====================================================
-# stuff that didn't work:
-# command = "'sinfo','--format=\"%n,%c, %Y,%m'"
-      # sinfo = subprocess.run([command])
-#####sinfo_data = subprocess.check_output(['sinfo','--format=[%n,%c,%Y,%m,%a,%O,%P]'])
-# array = []
-#i = 0
-#for line in sinfo_data:
-#   print(i)
-#   print(line)
-#   array = array.append(line)
-#   i += 1
-   # print(arg[1])
-# print ("rc = "+ str(sinfo_data.returncode))
-# print (sinfo_data)
-#print (array[3])
+import subprocess, array, json
+#sinfo -h --format="[%n--%c CPUs,%Y Cores|Memory=%m,State=%a,Load=%O,partition:%P\")" #human-readable
 #====================================================
 def node(self,name,cpus,cores,mem,state,load,partition):
     node = []
+node_list = []
 
 with open("sinfo.csv") as file: #csv format
            node_arr = [line.rstrip() for line in file.readlines()]
            # print (node_arr[7]+":")
            node_arr.remove(node_arr[0])  #header line
-           print ("----------------------")
+           # print ("----------------------")
            for index in range(len(node_arr)):
               my_names = []
               attrib_list = node_arr[index].split (",")
@@ -42,21 +24,25 @@ with open("sinfo.csv") as file: #csv format
               if attrib_list[5] == "N/A":
                     node.load = 0
               else:
-                    node.load = float(attrib_list[5])
+                    node.load = 0
+              #      node.load = (float( attrib_list[5]) / float(node.cores))
               node.partition = attrib_list[6]
 
               # if node.state == "down":
               if node.state in("down","down*"):
-                 node.color="black"
-              elif node.load<=.5:
-                 node.color='blue'
-              elif 1 < node.load:
-                 node.color='purple'
-              elif 1 < node.load1 < 2.5:
-                 node.color='magenta'
-              elif 2.5 <= node.load < 4:
-                 node.color='orange'
-              else:
-                 node.color='red'
-              print(node.name," is in group", node.group, "and is ",node.state," and load is ",node.load," and  color is ", node.color)
-
+                 # node.color="black"
+              # elif node.load<=.5:
+                 # node.color='blue'
+              # elif 1 < node.load:
+                 # node.color='purple'
+              # elif 1 < node.load < 2.5:
+                 # node.color='magenta'
+              # elif 2.5 <= node.load < 4:
+                 # node.color='orange'
+              # else:
+                 # node.color='red'
+              # print(node.name," is in group", node.group, "and is ",node.state," and load is ",node.load," and  color is ", node.color)
+                node_list.append(node.__dict__)
+              encoded_data = json.dumps( node_list )
+              print(encoded_data)
+ 
